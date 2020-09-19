5bc6fad7ff906324ae2a79c02a60f4ed16791da0
Poonam Bansal <poonam291997@gmail.com>
2020-09-20 00:49:46 +0530

    Create Solution.java

---
+++ p23/Solution.java
@@ -0,0 +1,17 @@
+void DoJob()
+{
+	Domain domain = new Domain();
+  try {
+    domain.fun();
+    domain.fun2(100);
+    Repostory rep = new Repository();
+		Emp emp = rep.get(1);
+  
+ } catch(e1) {
+ 
+ } catch(e2) {
+  
+  } catch(e3) {
+
+  }
+}
